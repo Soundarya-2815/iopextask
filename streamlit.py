67d6e24230e150994e39358f23419a392a557de2
Soundarya <75677805+Soundarya-2815@users.noreply.github.com>
2022-11-28 13:49:34 +0530

    Delete streamlit.py

--- streamlit.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from pymongo import MongoClient
-@st.experimental_singleton(suppress_st_warning=True)
-def init_connection():
-    return MongoClient("mongodb://localhost:27017/")
